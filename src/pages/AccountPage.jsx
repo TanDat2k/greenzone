b3b3058ab1b17dabfa3a0b0fe993a90395cb20b3
DESKTOP-S2VMJ1B\Phuc <phuc.thuong@accredoasia.com>
2023-05-19 14:39:20 +0700

    //

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -7,10 +7,9 @@ import { shortenAddress } from "../utils/shortenAddress";
 import { TransactionContext } from "../context/TransactionContext";
 const AccountPage = () => {
   const { currentAccount } = useContext(TransactionContext);
-
   return (
     <div className="flex flex-col w-full justify-center items-center bg-black">
-      <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
+      {/* <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
         <div className="flex justify-between flex-col w-full h-full">
           <div className="flex justify-between items-start">
             <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
@@ -28,8 +27,8 @@ const AccountPage = () => {
             <p className="text-white font-semibold text-lg mt-1">Ethereum </p>
           </div>
         </div>
-      </div>
-      {/* <Transactions /> */}
+      </div> */}
+      <Transactions />
     </div>
   );
 };
