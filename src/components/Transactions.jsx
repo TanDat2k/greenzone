f066ff792b4d0bae4bf40aa48677b81cdeb79130
DESKTOP-S2VMJ1B\Phuc <phuc.thuong@accredoasia.com>
2023-05-19 15:41:42 +0700

    //

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -18,25 +18,6 @@ const TransactionsCard = ({
 
   return (
     <>
-      <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
-        <div className="flex justify-between flex-col w-full h-full">
-          <div className="flex justify-between items-start">
-            <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
-              <SiEthereum fontSize={21} color="#fff" />
-            </div>
-            <BsInfoCircle fontSize={17} color="#fff" />
-          </div>
-          <div>
-            <a
-              className="text-white font-light text-sm"
-              href={`https://sepolia.etherscan.io/address/${currentAccount}`}
-            >
-              <p> {shortenAddress(currentAccount)}</p>
-            </a>
-            <p className="text-white font-semibold text-lg mt-1">Ethereum </p>
-          </div>
-        </div>
-      </div>
       <div
         className="bg-[#181918] m-4 flex flex-1
       2xl:min-w-[450px]
@@ -92,25 +73,46 @@ const Transactions = () => {
   const { transactions, currentAccount } = useContext(TransactionContext);
 
   return (
-    <div className="flex w-full justify-center items-center 2xl:px-20 bg-transparent">
-      <div className="flex flex-col md:p-12 py-12 px-4">
-        {currentAccount ? (
-          <h3 className="text-white text-3xl text-center my-2">
-            Danh sách giao dịch
-          </h3>
-        ) : (
-          <h3 className="text-white text-3xl text-center my-2">
-            Kết nối với tài khoản để xem giao dịch gần nhất
-          </h3>
-        )}
-
-        <div className="flex flex-wrap justify-center items-center mt-10">
-          {[...transactions].reverse().map((transaction, i) => (
-            <TransactionsCard key={i} {...transaction} />
-          ))}
+    <>
+      <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
+        <div className="flex justify-between flex-col w-full h-full">
+          <div className="flex justify-between items-start">
+            <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+              <SiEthereum fontSize={21} color="#fff" />
+            </div>
+            <BsInfoCircle fontSize={17} color="#fff" />
+          </div>
+          <div>
+            <a
+              className="text-white font-light text-sm"
+              href={`https://sepolia.etherscan.io/address/${currentAccount}`}
+            >
+              <p> {shortenAddress(currentAccount)}</p>
+            </a>
+            <p className="text-white font-semibold text-lg mt-1">Ethereum </p>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="flex w-full justify-center items-center 2xl:px-20 bg-transparent">
+        <div className="flex flex-col md:p-12 py-12 px-4">
+          {currentAccount ? (
+            <h3 className="text-white text-3xl text-center my-2">
+              Danh sách giao dịch
+            </h3>
+          ) : (
+            <h3 className="text-white text-3xl text-center my-2">
+              Kết nối với tài khoản để xem giao dịch gần nhất
+            </h3>
+          )}
+
+          <div className="flex flex-wrap justify-center items-center mt-10">
+            {[...transactions].reverse().map((transaction, i) => (
+              <TransactionsCard key={i} {...transaction} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
